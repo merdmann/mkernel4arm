6c5743d296b1d7c218d0f2a2ab9841ff93e692df
Michael Erdmann <michael.erdmann@snafu.de>
2015-05-30 21:33:56 +0200

    Changed the proc.c to match MISRA rules

--- trunk/target/kernel/proc.c
+++ trunk/target/kernel/proc.c
@@ -79,6 +79,58 @@ void _os_release_tcb(TaskType id) {
 	UserMode(_machdep_cas_byte( (POINTER)&_TCB[id].admin, _os_my_task_id, NULL_TASK_ID ));
 }
 
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param start [description]
+ * @return [description]
+ */
+static byte getComputable(byte start) {
+    byte i = 0;
+    byte current = start;
+    byte newState = PROC_FREE;
+
+    // find the next conputable task
+    for( i=0; i < _oil_max_tcb && newState == PROC_FREE; ++i) {
+        current = (start + i) % _oil_max_tcb;
+
+        /* tasks with this flags set are manipulated by a task */
+        if( _TCB[current].admin != NULL_TASK_ID || _TCB[current].state == PROC_SUSPENDED )
+            continue;
+
+        switch(_TCB[current].state) {
+            case PROC_WAIT_RESOURCE : {
+                    /* OSEK/VDX: we are waiting for an resource to become free */
+                    ResourceType p = (ResourceType)(_TCB[current].wait_addr);
+
+                    if( p->owner == NO_OWNER ) {
+                       p->owner = current;
+                       newState = PROC_COMPUTING;
+                    }
+                }
+                break;
+
+            case PROC_WAIT_EVENT:
+                /* OSEK/VDX: process wait for an set event flag, check for events */
+                if( _TCB[current].event & _TCB[current].mask )
+                    newState = PROC_COMPUTING;
+                break;
+
+             case PROC_COMPUTING:
+                newState = PROC_COMPUTING;
+                break;
+        } 
+    }
+
+    if( newState == PROC_FREE ) 
+        return _os_my_task_id;
+    else  {
+        _TCB[current].state = newState;
+        return current;
+    }
+}
+
 /*
  * This function is directly called either from the preemption
  * timer handler (_os_cpu_tick) or from the yield function.e Consequently
@@ -92,79 +144,17 @@ void _os_release_tcb(TaskType id) {
  *  user process.
  */
 void _os_schedule() {
-    byte start = 0;
-    t_task_id last_task_id;
-
     /* if non preemptable tasks is running or an admin task is pedning no recscheduling */
     if( (_TCB[_os_my_task_id].descriptor->schedule == NON && _TCB[_os_my_task_id].state == PROC_COMPUTING) 
         || _TCB[_os_my_task_id].admin != NULL_TASK_ID )
         return;
 
-    /* save the context by storing the current stack */
-    last_task_id = _os_my_task_id;
-    _TCB[last_task_id].stack = _os_current_stack;
-
-
-    /* define the startpoint for the scan */
-    if( _os_next_task != NULL_TASK_ID ) {
-      start = _os_next_task;
-      _os_next_task = NULL_TASK_ID;
-    }
-    else
-   	  start = 0;
+    _TCB[_os_my_task_id].stack = _os_current_stack;
 
     TaskEndHook(_os_my_task_id);
 
-    /*
-     * Scan all tasks and select an applicable task. TCB's are sorted according to
-     * priority of the task.
-     */
-    {
-       byte i = 0;
-       byte current = start;
-       byte state = PROC_FREE;
-
-       while ( i < _oil_max_tcb ) {
-    	     current = (start + i++) % _oil_max_tcb;
-           state = _TCB[current].state;
-
-           /* tasks with this flags set are manipulated by a task */
-           if( _TCB[current].admin != NULL_TASK_ID || state == PROC_SUSPENDED )
-        	     continue;
-
-           if( state == PROC_WAIT_RESOURCE ) {
-        	     /* OSEK/VDX: we are waiting for an resource to become free */
-               ResourceType p = (ResourceType)(_TCB[current].wait_addr);
-
-               if( p->owner == NO_OWNER ) {
-                   p->owner = current;
-         		       state = PROC_COMPUTING;
-               	   break;
-               }
-           }
-           else if( state == PROC_WAIT_EVENT ) {
-               /* OSEK/VDX: process wait for an set event flag, check for events */
-               if( _TCB[current].event & _TCB[current].mask ) {
-                   state = PROC_COMPUTING;
-                   break;
-               }
-           }
-           else if( state == PROC_COMPUTING ) {
-               break;
-           }
-       }
-       if( state == PROC_COMPUTING ) {
-    	     /*
-    	      * if the current task id has not changed we can assume that nobody else
-    	      * has executed schedule while we have searched for an executable task
-     	      */
-     	     if( _os_my_task_id == last_task_id ) {
-    		       _TCB[current].state = state;
-    		       _os_my_task_id = current;
-       		     _os_current_stack = _TCB[current].stack;
-    	     }
-       }
-    }
+    _os_my_task_id = getComputable( _os_my_task_id );
+    _os_current_stack = _TCB[_os_my_task_id].stack;
 
     TaskBeginHook(_os_my_task_id);
 }
