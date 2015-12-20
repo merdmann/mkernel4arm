9d609f3207ffa806075298ffa6b00a2cebeeece9
Michael Erdmann <michael.erdmann@snafu.de>
2015-12-20 20:03:00 +0100

    Changed to openocd 0.9.0

--- trunk/target/apps/myapp/dg.sh
+++ trunk/target/apps/myapp/dg.sh
@@ -1,5 +1,5 @@
-/c/tools/openocd-0.9.0/bin-x64/openocd.exe --file  interface/stlink-v2.cfg  -c "gdb_memory_map enable"  -c "gdb_flash_program enable"  --file target/stm32f4x_stlink.cfg &
-adg bin/myapp.elf
+/h/lib/openocd-0.9.0/bin-x64/openocd.exe --file  interface/stlink-v2.cfg  -c "gdb_memory_map enable"  -c "gdb_flash_program enable"  --file target/stm32f4x_stlink.cfg &
+adg ~/Sources/mkernel4arm/build/myapp/bin/myapp.elf
 
 pid=`ps | grep ocd | awk '{ print $1 }'`
 if [ "x${pid}" != "x" ] ; then
